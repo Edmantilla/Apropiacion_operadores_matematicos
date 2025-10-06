4a663fe961d9b4f58209cc615f2244b59a905c68
eduard mantilla <edmantilla159mail.com>
2025-10-06 17:46:20 -0500

    Se realizo el tercer punto con sus respectivos comentarios

---
+++ app3.js
@@ -0,0 +1,10 @@
+// Se declararon las variables a utilizar
+let contador=100,resultado;
+// Se realizaron las operaciones
+contador+=25;
+// Se realizaron las operaciones
+contador-=10;
+// Se realizaron las operaciones
+resultado=contador*2;
+// Se mostraron los resultados
+alert("El resultado final es: "+resultado);
