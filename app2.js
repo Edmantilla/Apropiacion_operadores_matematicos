ba4e3bee33c29e4bb74478565184ab9301b4dbe5
eduard mantilla <edmantilla159mail.com>
2025-10-06 17:30:44 -0500

    Se realizo el segundo ejercicio

---
+++ app2.js
@@ -0,0 +1,10 @@
+// Se declararon las variables autilizar
+let nota1,nota2,nota3,promedio;
+// Se solicitaron los datos al usuario
+nota1=parseFloat(prompt("Ingrese la primera nota:"));
+nota2=parseFloat(prompt("Ingrese la segunda nota:"));
+nota3=parseFloat(prompt("Ingrese la tercera nota:"));
+// Se realizaron los calculos
+promedio=(nota1+nota2+nota3)/3;
+// Se mostraron los resultados
+alert("El promedio de las notas es: "+promedio);
