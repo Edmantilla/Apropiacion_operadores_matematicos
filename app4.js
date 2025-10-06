ef08cd825f26eb18c405df37617f17db4740f851
eduard mantilla <edmantilla159mail.com>
2025-10-06 18:01:31 -0500

    Se realizo el 4 ejercicio y la operacion que se realiza

---
+++ app4.js
@@ -0,0 +1,7 @@
+// Se declararon las variables a utilizar
+let operaciones,parentesis,multiplicacion,suma,resta,division;
+// Se realizaron las operaciones
+operaciones= (20+10)/5+3*2-4*(20 + 10) / 5 + 3 * 2 - 4*(20+10)/5+3*2-4
+
+// Se mostraron los resultados
+alert("El resultado de las operaciones es: "+operaciones);
